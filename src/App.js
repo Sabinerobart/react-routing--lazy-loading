6393add9529dd2a0ce0e108bf4325564a330dd97
Sabine Robart <sabine.robart@protonmail.com>
2020-03-14 14:30:58 +0100

    Add toggle button to only load the required component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import React, { Component, Suspense } from 'react';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+// import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
 import User from './containers/User';
-import Welcome from './containers/Welcome';
+// import Welcome from './containers/Welcome';
 // import Posts from './containers/Posts';
 const Posts = React.lazy(() => import('./containers/Posts'));
 
 class App extends Component {
+  state = { showPosts: false };
+
+  modeHandler = () => {
+    this.setState(prevState => {
+      return { showPosts: !prevState.showPosts }
+    })
+  }
   render() {
     return (
-      <BrowserRouter>
-        <React.Fragment>
-          <nav>
-            <NavLink to="/user">User Page</NavLink> |&nbsp;
-            <NavLink to="/posts">Posts Page</NavLink>
-          </nav>
-          <Route path="/" component={Welcome} exact />
-          <Route path="/user" component={User} />
-          {/* <Route path="/posts" component={Posts} /> */}
-          <Route path="/posts" render={() => <Suspense fallback={<div>Loading...</div>}><Posts /></Suspense>} />
+      <>
+        <button onClick={this.modeHandler}>Toggle Mode</button>
+        {this.state.showPosts ? (
+          <Suspense fallback={<div>Loading...</div>}><Posts /></Suspense>
+        ) : <User />
+        }
 
-        </React.Fragment>
-      </BrowserRouter>
+        {/* 
+          <BrowserRouter>
+            <React.Fragment>
+              <nav>
+                <NavLink to="/user">User Page</NavLink> |&nbsp;
+                <NavLink to="/posts">Posts Page</NavLink>
+              </nav>
+              <Route path="/" component={Welcome} exact />
+              <Route path="/user" component={User} />
+              <Route path="/posts" component={Posts} /> 
+            </React.Fragment>
+         </BrowserRouter>
+        */}
+      </>
     );
   }
 }
